061cf3152a5b2cd28a9157be051dcd26145fc953
hmelo1 <heriberto@melo.nyc>
2017-10-14 16:51:43 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,13 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+function lastImageSelector(){
+  return $('img:last')
+}
+function ninjaBabySelector(){
+  return $("img[id='baby-ninja']")
+}
+function divSelector(){
+  return $('div')
+}
